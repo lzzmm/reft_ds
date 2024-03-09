508a67c7b2dda5ecac241982e0261c356cdcb5aa
Chen Yuhan <ychen906@connect.hkust-gz.edu.cn>
2024-03-10 01:26:25 +0800

    Initial commit

---
+++ DeepSpeed/deepspeed/runtime/checkpoint_engine/temp.py
@@ -0,0 +1,250 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+
+import torch
+from deepspeed.utils import logger, log_dist
+from deepspeed.runtime.checkpoint_engine.checkpoint_engine import \
+    CheckpointEngine
+    
+from concurrent.futures import ThreadPoolExecutor
+import os
+import asyncio
+import aiofiles
+import threading
+import io
+from datetime import datetime
+import time
+from torch.profiler import profile, record_function
+
+
+class TorchCheckpointEngine(CheckpointEngine):
+
+    def __init__(self, config_params=None):
+        super().__init__(config_params)
+
+    def create(self, tag):
+        log_dist(f"[Torch] Checkpoint {tag} is about to be saved!", ranks=[0])
+
+    def save(self, state_dict, path: str):
+        logger.info(f"[Torch] Saving {path}...")
+        torch.save(state_dict, path)
+        logger.info(f"[Torch] Saved {path}.")
+        return None
+
+    def load(self, path: str, map_location=None):
+        logger.info(f"[Torch] Loading checkpoint from {path}...")
+        partition = torch.load(path, map_location=map_location)
+        logger.info(f"[Torch] Loaded checkpoint from {path}.")
+        return partition
+
+    def commit(self, tag):
+        logger.info(f"[Torch] Checkpoint {tag} is ready now!")
+        return True
+    
+    
+class AsyncCheckpointEngine(CheckpointEngine):
+    
+    def __init__(self, config_params=None):
+        super().__init__(config_params)
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        # self.allreduce_semaphore = asyncio.Semaphore(1)
+        self.thread_lock = threading.Lock()
+        self.state_dict_cpu = {}
+
+    def create(self, tag):
+        log_dist(f"[AsyncCkpt] Checkpoint {tag} is about to be saved!", ranks=[0])
+
+    def save(self, state_dict, path: str, snapshot_=True, use_copy_=True, snapshot_stream=torch.cuda.Stream(), device='cuda:0'): # f'cuda:{mpu.get_data_parallel_rank()}'
+        start_time = time.time()
+        self.state_dict_cpu = {}
+        if snapshot_:
+            self.make_snapshot(state_dict, use_copy_, snapshot_stream, device)
+            self.path = path
+            # self.save_checkpoint(self.state_dict_cpu, path, snapshot_stream, start_time)
+        else:
+            self.save_checkpoint(state_dict, path, start_time)
+        logger.info(f"[AsyncCkpt] Saved {path}.")
+        return None
+
+    def load(self, path: str, map_location=None):
+        logger.info(f"[AsyncCkpt] Loading checkpoint from {path}...")
+        partition = torch.load(path, map_location=map_location)
+        logger.info(f"[AsyncCkpt] Loaded checkpoint from {path}.")
+        return partition
+
+    def commit(self, tag):
+        logger.info(f"[AsyncCkpt] Checkpoint {tag} is ready now!")
+        return True
+    
+    def make_snapshot(self, state_dict, use_copy_, snapshot_stream, device):
+        logger.info(f"[AsyncCkpt] Snapshoting...")
+        # with self.thread_lock:
+        #     asyncio.run(self.allreduce_semaphore.acquire())
+
+        snapshot_thread = threading.Thread(
+            target=self._snapshot_thread,
+            args=(state_dict, use_copy_, snapshot_stream, device)
+        )
+        snapshot_thread.start()
+        return snapshot_thread
+    
+    def save_checkpoint(self, state_dict, path: str, snapshot_stream, start_time):
+        
+        # asyncio.run(self.allreduce_semaphore.acquire())
+        
+        checkpoint_thread = threading.Thread(
+            target=self._checkpoint_thread,
+            args=(state_dict, path, snapshot_stream, start_time)
+        )
+        checkpoint_thread.start()
+
+    
+    def snapshot_and_save():
+        pass
+        
+    def _snapshot_thread(self, state_dict, use_copy_, snapshot_stream, device):
+        # if use_timer and step_cnt > 10:
+        start_time = time.perf_counter()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_until_complete(self._make_snapshot(state_dict, use_copy_, snapshot_stream, device))
+        finally:
+            loop.close()
+        # if use_timer and step_cnt > 10:
+        end_time = time.perf_counter()
+        print(f"snapshot time: {end_time - start_time}\n")
+        # torch.save(self.state_dict_cpu, self.path)        # if snapshot_stream is not None:
+        #     snapshot_stream.synchronize()
+        # self.save_checkpoint(self.state_dict_cpu, self.path, snapshot_stream, start_time)
+        #     timer_record_file.write(f"step: {step_cnt}\n")
+        # timer_record_file.write(f"snapshot time: {end_time - start_time}\n")
+    
+    async def _make_snapshot_rec(self, state_dict, use_copy_, snapshot_stream, device):
+        def _copy_tensors_to_cpu(data, use_copy_):
+            if isinstance(data, dict):
+                cpu_data = {}
+                for key, value in data.items():
+                    if isinstance(value, torch.Tensor):
+                        if use_copy_:
+                            tensor_cpu = torch.empty_like(value, device='cpu').pin_memory()
+                            tensor_cpu.copy_(value, non_blocking=True)
+                            cpu_data[key] = tensor_cpu
+                        else:
+                            cpu_data[key] = value.cpu()
+                    elif isinstance(value, (dict, list)):
+                        cpu_data[key] = _copy_tensors_to_cpu(value, use_copy_)
+                    else:
+                        cpu_data[key] = value
+                return cpu_data
+            elif isinstance(data, list):
+                cpu_data = []
+                for item in data:
+                    if isinstance(item, torch.Tensor):
+                        if use_copy_:
+                            tensor_cpu = torch.empty_like(item, device='cpu').pin_memory()
+                            tensor_cpu.copy_(item, non_blocking=True)
+                            cpu_data.append(tensor_cpu)
+                        else:
+                            cpu_data.append(item.cpu())
+                    elif isinstance(item, (dict, list)):
+                        cpu_data.append(_copy_tensors_to_cpu(item, use_copy_))
+                    else:
+                        cpu_data.append(item)
+                return cpu_data
+            else:
+                return data
+                        
+        snapshot_stream.wait_stream(torch.cuda.default_stream(device))
+        with torch.cuda.stream(snapshot_stream):
+            self.state_dict_cpu = _copy_tensors_to_cpu(state_dict, use_copy_)
+            
+            
+    async def _make_snapshot(self, state_dict, use_copy_, snapshot_stream, device):
+        def _copy_tensors_to_cpu(data, use_copy_):
+            stack = [(data, None, None)]
+            root = None
+            
+            while stack:
+                current, parent, key = stack.pop()
+                if isinstance(current, torch.Tensor):
+                    if use_copy_:
+                        tensor_cpu = torch.empty_like(current, device='cpu').pin_memory()
+                        tensor_cpu.copy_(current, non_blocking=True)
+                        result = tensor_cpu
+                    else:
+                        result = current.cpu()
+                    if parent is not None:
+                        parent[key] = result
+                    else:
+                        root = result 
+                elif isinstance(current, dict):
+                    cpu_data = {}
+                    for k, v in current.items():
+                        stack.append((v, cpu_data, k))
+                    if parent is not None:
+                        parent[key] = cpu_data
+                    else:
+                        root = cpu_data
+                elif isinstance(current, list):
+                    cpu_data = [None] * len(current)
+                    for idx, item in enumerate(current):
+                        stack.append((item, cpu_data, idx))
+                    if parent is not None:
+                        parent[key] = cpu_data
+                    else:
+                        root = cpu_data
+                else:
+                    if parent is not None:
+                        parent[key] = current
+                    else:
+                        root = current
+            
+            return root
+
+        snapshot_stream.wait_stream(torch.cuda.default_stream(device))
+        with torch.cuda.stream(snapshot_stream):
+            self.state_dict_cpu = _copy_tensors_to_cpu(state_dict, use_copy_)
+        
+                        
+    def _checkpoint_thread(self, state_dict, path, snapshot_stream, start_time):
+        # if snapshot_stream is not None:
+        #     snapshot_stream.synchronize()
+        logger.info(f"[AsyncCkpt] Saving {path}...")
+        # torch.save(state_dict, path)
+        start_time = time.time()
+        
+        timestamp = datetime.now().strftime('%H:%M:%S')
+        print(f"[{timestamp}] save_checkpoint started...")
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_until_complete(self._save_checkpoint(state_dict, path))
+        finally:
+            loop.close()
+        
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("path", path)
+        ckpt_size = os.path.getsize(path) / (1024 * 1024)  # MB
+        speed = ckpt_size / elapsed_time
+        timestamp = datetime.now().strftime('%H:%M:%S')
+        print(f"[{timestamp}] Checkpoint size {ckpt_size:.2f} MB, speed {speed:.2f} MB/s, elapsed {elapsed_time:.2f} sec.")
+
+    async def _save_checkpoint(self, state_dict, path):
+        # state = {
+        #     'model': model.state_dict(),
+        #     'optimizer': optimizer.state_dict()
+        # }
+        buffer = io.BytesIO()
+        current_loop = asyncio.get_event_loop()
+        # self.allreduce_semaphore.release()
+        
+        await current_loop.run_in_executor(self.executor, torch.save, state_dict, buffer)
+        buffer.seek(0)
+        async with aiofiles.open(path, "wb") as f:
+            await f.write(buffer.read())
+        # timestamp = datetime.now().strftime('%H:%M:%S')
+        # print(f"[{timestamp}] saved")
---
+++ README.md
@@ -0,0 +1,78 @@
+ds_report
+
+```sh
+--------------------------------------------------
+DeepSpeed C++/CUDA extension op report
+--------------------------------------------------
+NOTE: Ops not installed will be just-in-time (JIT) compiled at
+      runtime if needed. Op compatibility means that your system
+      meet the required dependencies to JIT install the op.
+--------------------------------------------------
+JIT compiled ops requires ninja
+ninja .................. [OKAY]
+--------------------------------------------------
+op name ................ installed .. compatible
+--------------------------------------------------
+ [WARNING]  async_io requires the dev libaio .so object and headers but these were not found.
+ [WARNING]  async_io: please install the libaio-devel package with yum
+ [WARNING]  If libaio is already installed (perhaps from source), try setting the CFLAGS and LDFLAGS environment variables to where it can be found.
+async_io ............... [NO] ....... [NO]
+fused_adam ............. [NO] ....... [OKAY]
+cpu_adam ............... [NO] ....... [OKAY]
+cpu_adagrad ............ [NO] ....... [OKAY]
+cpu_lion ............... [NO] ....... [OKAY]
+ [WARNING]  Please specify the CUTLASS repo directory as environment variable $CUTLASS_PATH
+evoformer_attn ......... [NO] ....... [NO]
+fused_lamb ............. [NO] ....... [OKAY]
+fused_lion ............. [NO] ....... [OKAY]
+inference_core_ops ..... [NO] ....... [OKAY]
+cutlass_ops ............ [NO] ....... [OKAY]
+transformer_inference .. [NO] ....... [OKAY]
+quantizer .............. [NO] ....... [OKAY]
+ragged_device_ops ...... [NO] ....... [OKAY]
+ragged_ops ............. [NO] ....... [OKAY]
+random_ltd ............. [NO] ....... [OKAY]
+ [WARNING]  sparse_attn requires a torch version >= 1.5 and < 2.0 but detected 2.0
+ [WARNING]  using untested triton version (2.0.0), only 1.0.0 is known to be compatible
+sparse_attn ............ [NO] ....... [NO]
+spatial_inference ...... [NO] ....... [OKAY]
+transformer ............ [NO] ....... [OKAY]
+stochastic_transformer . [NO] ....... [OKAY]
+--------------------------------------------------
+DeepSpeed general environment info:
+torch install path ............... ['/home/datasets/yxwang/miniconda3/lib/python3.11/site-packages/torch']
+torch version .................... 2.0.1+cu117
+deepspeed install path ........... ['/home/datasets/yxwang/miniconda3/lib/python3.11/site-packages/deepspeed']
+deepspeed info ................... 0.13.5+4578c249, 4578c249, HEAD
+torch cuda version ............... 11.7
+torch hip version ................ None
+nvcc version ..................... 11.7
+deepspeed wheel compiled w. ...... torch 2.0, cuda 11.7
+shared memory (/dev/shm) size .... 251.53 GB
+```
+
+Install modified DeepSpeed
+
+```sh
+cd DeepSpeed
+pip install .
+```
+
+Run Megatron-DeepSpeed
+
+Firstly configure `Megatron-DeepSpeed/examples_deepspeed/bert_with_pile/ds_pretrain_bert_copy.sh`
+
+line 9 global_batch_size
+line 95 batch_size  Make sure that batch_size <= global_batch_size*pp_size*mp_size/num_gpus
+line 85 / 86 GPU num
+line 104 num_save
+line 123 data_home
+line 153 output_home
+
+Then run
+
+```sh
+./ds_pretrain_bert_copy.sh
+```
+
+Thanks!
