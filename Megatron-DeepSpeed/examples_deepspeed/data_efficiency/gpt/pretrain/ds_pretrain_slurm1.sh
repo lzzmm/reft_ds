7b1625f94c059abacd68e4f31629de70e6721f70
Kang Xueze <leonkangt@gmail.com>
2024-05-13 09:52:32 +0800

    merge conficts solved

--- Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/pretrain/ds_pretrain_slurm1.sh
+++ Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/pretrain/ds_pretrain_slurm1.sh
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-srun --pty --partition=real --job-name=slurm_test --nodes=1 \
---ntasks-per-node=8 --cpus-per-task=4 --gres=gpu:8 \
---nodelist=hkbugpusrv04 bash ds_pretrain_slurm_run.sh
-=======
 srun --pty --partition=i64m1tga40u --job-name=slurm_test --nodes=2 \
 --ntasks-per-node=1 --cpus-per-task=4 --gres=gpu:2 \
 --nodelist=gpu3-11,gpu3-12 bash ds_pretrain_slurm_run.sh
->>>>>>> parallel_test
 
