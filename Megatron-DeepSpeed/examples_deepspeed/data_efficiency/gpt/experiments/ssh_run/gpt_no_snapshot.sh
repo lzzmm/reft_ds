01814eb105640119946f83e5e34927c0f06144e1
Kang Xueze <leonkangt@gmail.com>
2024-06-17 20:54:54 +0800

    abci

--- Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/experiments/ssh_run/gpt_no_snapshot.sh
+++ Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/experiments/ssh_run/gpt_no_snapshot.sh
@@ -1,4 +1,4 @@
-./ds_pretrain_gpt.sh model_size_config=3 checkpoint_new_thread="true" checkpoint_new_stream="true" \
+../ds_pretrain_gpt.sh model_size_config=3 checkpoint_new_thread="true" checkpoint_new_stream="true" \
                      enable_parity="false" enable_pin_memory="true" enable_sharding="true" \
                      enable_profile="true" enable_save="false" save_location="nfs" enable_snapshot="false" \
                      prealloc="true" pure_torch_save="false" get_state_dict_shape="false" \
--- Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/experiments/ssh_run/gpt_sync_snapshot.sh
+++ Megatron-DeepSpeed/examples_deepspeed/data_efficiency/gpt/experiments/ssh_run/gpt_sync_snapshot.sh
@@ -1,4 +1,4 @@
-./ds_pretrain_gpt.sh model_size_config=8 checkpoint_new_thread="false" checkpoint_new_stream="false" \
+../ds_pretrain_gpt.sh model_size_config=8 checkpoint_new_thread="false" checkpoint_new_stream="false" \
                      enable_parity="false" enable_pin_memory="true" enable_sharding="false" \
                      enable_profile="true" enable_save="false" save_location="nfs" enable_snapshot="true" \
                      prealloc="true" pure_torch_save="true" get_state_dict_shape="false" \
